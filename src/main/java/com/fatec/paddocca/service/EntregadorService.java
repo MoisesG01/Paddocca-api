f63769aeec05b8022804fcf2a39a474d4f69b0b9
Moises Goncalves <moises_goncalves@outlook.com.br>
2024-06-06 20:16:11 -0300

    Separação das Camadas e implementação de validação de email

--- src/main/java/com/fatec/paddocca/service/EntregadorService.java
+++ src/main/java/com/fatec/paddocca/service/EntregadorService.java
@@ -3,7 +3,7 @@ package com.fatec.paddocca.service;
 import com.fatec.paddocca.model.entity.Entregador;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.fatec.paddocca.model.repository.EntregadorRepository;
+import com.fatec.paddocca.repository.EntregadorRepository;
 
 @Service
 public class EntregadorService {
